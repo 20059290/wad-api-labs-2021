dd5e37b742fe4cb963bc7590d2e81043ed61bc91
Stephen Carbery <carbery.stephen@gmail.com>
2021-11-22 03:55:34 +0000

    Mongo lab Users Router

--- movies-api/index.js
+++ movies-api/index.js
@@ -4,6 +4,7 @@ import moviesRouter from './api/movies';
 import genresRouter from './api/genres';
 import './db';
 import './seedData';
+import usersRouter from './api/users';
 
 dotenv.config();
 
@@ -12,8 +13,12 @@ const app = express();
 const port = process.env.PORT;
 
 app.use(express.json());
+//Movies router
 app.use('/api/movies', moviesRouter);
+//Genres router
 app.use('/api/genres', genresRouter);
+//Users router
+app.use('/api/users', usersRouter);
 
 app.listen(port, () => {
   console.info(`Server running at ${port}`);
