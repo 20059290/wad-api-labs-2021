14b24026febdd259588a762089cccafd42504f06
Stephen Carbery <carbery.stephen@gmail.com>
2021-12-05 21:06:16 +0000

    Topic 10 - JWT

--- movies-api/api/users/userModel.js
+++ movies-api/api/users/userModel.js
@@ -32,11 +32,11 @@ const UserSchema = new Schema({
     if (this.isModified('password') || this.isNew) {
         bcrypt.genSalt(10, (err, salt)=> {
             if (err) {
-                return next(err);
+              return next(err);
             }
             bcrypt.hash(user.password, salt, null, (err, hash)=> {
                 if (err) {
-                    return next(err);
+                  return next(err);
                 }
                 user.password = hash;
                 next();
