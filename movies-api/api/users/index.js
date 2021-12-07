297992500ddc7e135544e399ab3c9dc997566f9e
Stephen Carbery <carbery.stephen@gmail.com>
2021-12-07 18:22:15 +0000

    Topic 10 Exercises

--- movies-api/api/users/index.js
+++ movies-api/api/users/index.js
@@ -5,6 +5,7 @@ import jwt from 'jsonwebtoken';
 import movieModel from '../movies/movieModel';
 
 const router = express.Router(); // eslint-disable-line
+var pwRegex = new RegExp("^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{5,}$");
 
 // Get all users
 router.get('/', async (req, res) => {
@@ -19,22 +20,28 @@ router.post('/',asyncHandler( async (req, res, next) => {
       return next();
     }
     if (req.query.action === 'register') {
-      await User.create(req.body);
-      res.status(201).json({code: 201, msg: 'Successful created new user.'});
-    } else {
-      const user = await User.findByUserName(req.body.username);
-        if (!user) return res.status(401).json({ code: 401, msg: 'Authentication failed. User not found.' });
-        user.comparePassword(req.body.password, (err, isMatch) => {
-          if (isMatch && !err) {
-            // if user is found and password matches, create a token
-            const token = jwt.sign(user.username, process.env.SECRET);
-            // return the information including token as JSON
-            res.status(200).json({success: true, token: 'BEARER ' + token});
-          } else {
-            res.status(401).json({code: 401,msg: 'Authentication failed. Wrong password.'});
-          }
-        });
-      }
+        if (pwRegex.test(req.body.password)){
+            await User.create(req.body);
+            res.status(201).json({code: 201, msg: 'Successful created new user.'});
+            } 
+            else{
+                res.status(401).json({code: 401,msg: 'Bad Password'});
+            }
+        }
+        else {
+        const user = await User.findByUserName(req.body.username);
+            if (!user) return res.status(401).json({ code: 401, msg: 'Authentication failed. User not found.' });
+            user.comparePassword(req.body.password, (err, isMatch) => {
+            if (isMatch && !err) {
+                // if user is found and password matches, create a token
+                const token = jwt.sign(user.username, process.env.SECRET);
+                // return the information including token as JSON
+                res.status(200).json({success: true, token: 'BEARER ' + token});
+            } else {
+                res.status(401).json({code: 401,msg: 'Authentication failed. Wrong password.'});
+            }
+            });
+        }
   }));
 
   // Update a user
@@ -56,9 +63,14 @@ router.post('/:userName/favourites', asyncHandler(async (req, res) => {
     const userName = req.params.userName;
     const movie = await movieModel.findByMovieDBId(newFavourite);
     const user = await User.findByUserName(userName);
-    await user.favourites.push(movie._id);
-    await user.save(); 
-    res.status(201).json(user); 
+    if (!user.favourites.includes(movie._id)){
+        await user.favourites.push(movie._id);
+        await user.save(); 
+        res.status(201).json(user); 
+    }
+    else{
+        res.status(401).json({ code: 401, msg: 'This movie is already in this users favourites' });
+    }
   }));
 
 router.get('/:userName/favourites', asyncHandler( async (req, res) => {
